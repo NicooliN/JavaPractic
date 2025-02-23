3979af5ded60f96c8f0f9a957abeae80793398eb
NicooiN <niko91@tpu.ru>
2025-02-23 15:48:33 +0300

    #develop! add task simple create thread

--- src/day4/concurrency/App.java
+++ src/day4/concurrency/App.java
@@ -2,16 +2,26 @@ package day4.concurrency;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("MAin thread=" + getThredInfo());
 
+        Runnable task = () -> {
+            System.out.println("Task thread = " + getThreadInfo());
+        };
+
+        Thread thread = new Thread(task);
+        thread.start();
+
+        thread = new Thread(task);
+        thread.start();
+
+        System.out.println("Main thread = " + getThreadInfo());
     }
 
-    private static String getThredInfo() {
+    private static String getThreadInfo() {
         Thread thread =Thread.currentThread();
         long id = thread.getId();
         String name = thread.getName();
 
-       return "Thread id = " + id + " Thread Name = " + name;
+       return "Thread{id = " + id + "; Name = " + name + "}";
     }
 
 
